4f8e42fe94c92b5656aacbbb6ca494f14890dd61
tamir321 <tamir321@gmail.com>
2022-12-01 16:12:16 +0200

    add response object

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
-from typing import Union
+from typing import Union, List
 from fastapi import FastAPI
 from models.item import Item
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# 325408706
 app = FastAPI()
 
 a = []
@@ -27,18 +13,11 @@ z1 = Item(name="jnk", price=7, is_offer=True)
 a.append(z)
 a.append(z1)
 
-print(a)
-print(z.name)
-print(z.json())
 
 with open('data.json', 'r') as f:
     data = json.load(f)
-# print(data)
-# print(type(data))
-print(len(data))
 
 for kkk in data:
-    print(kkk)
     ddd = Item(**kkk)
     a.append(ddd)
 
@@ -48,7 +27,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/jnk/")
+@app.get("/jnk/",response_model=List[Item])
 def read_root():
     return a
 
